d8f380d34b5b4c741cd9e021309f78015ee0945a
Brad Sokol <bsokol@gmail.com>
2010-09-17 14:15:33 -0400

    Progress on integration tests

---
+++ src/IntegrationTests/test.js
@@ -0,0 +1,23 @@
+//UIALogger.logStart("Starting Test");
+//  var view = UIATarget.localTarget().frontMostApp().mainWindow().elements()[2];
+//  var textfields = view.textFields();
+//  if (textfields.length != 2) {
+//	  UIALogger.logFail("Wrong number of text fields");
+//  } else {
+//	  UIALogger.logPass("Right number of text fields");
+//  }
+//  var passwordfields = view.secureTextFields();
+//  if (passwordfields.length != 1) {
+//	  UIALogger.logFail("Wrong number of password fields");
+//  } else {
+//	  UIALogger.logPass("Right number of password fields");
+//  }
+//  textfields["username"].setValue("tturner");
+//  passwordfields[0].setValue("tod");
+//  view.buttons()["logon"].tap();
+//  var errorVal = view.staticTexts()["error"].value();
+//  if (errorVal != "Invalid User Name or Password") {
+//	  UIALogger.logFail("Did Not Get Invalid Username Error: " + errorVal);
+//  } else {
+//	  UIALogger.logPass("Username Error Detected");
+//  }
