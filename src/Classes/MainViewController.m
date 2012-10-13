9f3afd2b088da0dc455b70c7ffd1ce210e34d3e8
Brad Sokol <bsokol@gmail.com>
2012-10-13 17:05:30 -0400

    Update camera/lens description on main view when flipside view is dismissed. Github issue 5.

--- src/Classes/MainViewController.m
+++ src/Classes/MainViewController.m
@@ -762,6 +762,8 @@ static BOOL previousLensWasZoom = YES;
 - (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+    
+    [self updateCameraAndLensDescription];
 }
 
 #pragma mark UIActionSheetDelegate
