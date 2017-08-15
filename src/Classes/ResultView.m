079eac48fc21fe4874a243a8729ed5557fa81032
Brad Sokol <bsokol@gmail.com>
2017-08-15 18:40:55 -0400

    Fix copyright

--- src/Classes/ResultView.m
+++ src/Classes/ResultView.m
@@ -1,4 +1,4 @@
-// Copyright 2009 Brad Sokol
+// Copyright 2009-2017 Brad Sokol
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@
 //  eliminates this delay.
 //
 //  Created by Brad on 2009/03/19.
-//  Copyright 2009 Brad Sokol. All rights reserved.
+//  Copyright 2009-2017 Brad Sokol. 
 //
 
 #import "ResultView.h"
